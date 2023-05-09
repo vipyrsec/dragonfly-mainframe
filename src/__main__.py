607ee42d2c2399f6b298438fc43c28fecbf573b2
jz9 <69145546+jonathan-d-zhang@users.noreply.github.com>
2023-05-09 19:44:10 -0400

    Remove scaffolding
    It's changed almost completely anyway.

--- src/__main__.py
+++ src/__main__.py
@@ -10,21 +10,3 @@ load_dotenv()
 engine = create_async_engine("postgresql+asyncpg://postgres:postgres@localhost:5432")
 app = FastAPI()
 
-
-@app.post("/queue-package")
-async def queue_package():
-    """Add a package to the database to be scanned later."""
-
-
-@app.get("/package")
-async def give_work():
-    """Find an unscanned package and return it."""
-    async with AsyncSession(engine) as session:
-        data = await session.execute(select(Package))
-        for row in data.scalars():
-            print(row)
-
-
-@app.post("/package")
-async def update_verdict():
-    """Update the database with the result of a package scan."""
