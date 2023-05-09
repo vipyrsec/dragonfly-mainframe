c5c535a1687f8be9595b458f4fe148d9c8c39e90
jz9 <69145546+jonathan-d-zhang@users.noreply.github.com>
2023-05-09 14:51:01 -0400

    Add alembic for migrations
    Switch to postgres

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,17 @@
+import os
+
+from dotenv import load_dotenv
 from fastapi import FastAPI
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import Session
+
+from .models import Package
+
+load_dotenv()
 
 app = FastAPI()
+
+engine = create_engine(os.getenv("DB_URL", "sqlite:///:memory:"))
 
 
 @app.post("/queue-package")
@@ -11,6 +22,9 @@ def queue_package():
 @app.get("/package")
 def give_work():
     """Find an unscanned package and return it."""
+    with Session(engine) as session:
+        print(session)
+        print(select(Package))
 
 
 @app.post("/package")
