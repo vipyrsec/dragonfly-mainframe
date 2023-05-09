cca4f1b74a86dbe68395c65a3e098ed21dba682e
jz9 <69145546+jonathan-d-zhang@users.noreply.github.com>
2023-05-09 10:13:51 -0400

    Add `queue_package` endpoint for queuing packages
    endpoint may change in the future

--- src/__main__.py
+++ src/__main__.py
@@ -3,6 +3,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+@app.post("/queue-package")
+def queue_package():
+    """Add a package to the database to be scanned later."""
+
+
 @app.get("/package")
 def give_work():
     """Find an unscanned package and return it."""
