52ceb2091611bd11283b36e369de71cdcb4390b2
jz9 <69145546+jonathan-d-zhang@users.noreply.github.com>
2023-05-09 21:40:23 -0400

    Actually commit results of update

--- src/mainframe/__main__.py
+++ src/mainframe/__main__.py
@@ -1,5 +1,6 @@
 import uuid
 from dataclasses import dataclass
+from typing import Optional
 
 from dotenv import load_dotenv
 from fastapi import FastAPI
@@ -16,26 +17,26 @@ app = FastAPI()
 
 @dataclass(frozen=True)
 class PackageScanResult:
-    most_malicious_file: str | None
+    most_malicious_file: Optional[str]
     score: int
 
 
 @app.put("/package/{package_id}")
 async def update_verdict(pack_res: PackageScanResult, package_id: uuid.UUID):
-    """"""
+    """Update database with results of package scan."""
     print(pack_res)
-
     async with AsyncSession(engine) as session:
         await session.execute(
             update(Package)
             .where(Package.package_id == package_id)
             .values(most_malicious_file=pack_res.most_malicious_file, score=pack_res.score, status=Status.FINISHED)
         )
+        await session.commit()
 
 
 @app.get("/package/{package_id}")
 async def package_info(package_id: uuid.UUID):
-    """"""
+    """Get information about a package"""
     async with AsyncSession(engine) as session:
         data = await session.execute(select(Package).filter_by(package_id=package_id))
         return data.scalar_one()
