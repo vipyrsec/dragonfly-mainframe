1ff019cc60cea61c33ae6d14336bad8a282c97e7
jz9 <69145546+jonathan-d-zhang@users.noreply.github.com>
2023-05-14 22:56:06 -0400

    Extract test data to `tests/test_data/`

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,16 @@
 import datetime as dt
+import json
 import subprocess
 import sys
 import time
+import uuid
+from pathlib import Path
+from typing import Generator
 
 import pytest
 import requests
 from sqlalchemy import Engine, create_engine, insert
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session, sessionmaker
 
 from mainframe.models.orm import Base, Package, Status
 
@@ -27,81 +31,54 @@ else:
     sys.exit(1)
 
 
+TEST_DIR = Path(__file__).parent
+TEST_DATA_DIR = TEST_DIR / "test_data"
+TEST_DATA_FILES = list(TEST_DATA_DIR.iterdir())
+
+
 @pytest.fixture(scope="session")
-def engine():
+def engine() -> Engine:
     return create_engine("postgresql://postgres:postgres@db:5432")
 
 
 @pytest.fixture(scope="session")
-def sm(engine):
+def sm(engine) -> sessionmaker:
     return sessionmaker(bind=engine, autoflush=True)
 
 
 @pytest.fixture
-def api_url():
+def api_url() -> str:
     return _api_url
 
 
-@pytest.fixture
-def test_data():
-    return [
-        dict(
-            package_id="fce0366b-0bcf-4a29-a0a7-4d4bdf3c6f61",
-            name="a",
-            version="0.1.0",
-            status=Status.FINISHED,
-            score=0,
-            inspector_url="main.py",
-            queued_at=dt.datetime(2023, 5, 12, 18),
-            pending_at=dt.datetime(2023, 5, 12, 18, 30),
-            finished_at=dt.datetime(2023, 5, 12, 19),
-            client_id="remmy",
-            reported_at=None,
-        ),
-        dict(
-            package_id="df157a3c-8994-467f-a494-9d63eaf96564",
-            name="b",
-            version="0.1.0",
-            status=Status.FINISHED,
-            score=None,
-            inspector_url=None,
-            queued_at=dt.datetime(2023, 5, 12, 15),
-            pending_at=dt.datetime(2023, 5, 12, 16),
-            finished_at=dt.datetime(2023, 5, 12, 16, 30),
-            client_id="remmy",
-            reported_at=dt.datetime(2023, 5, 12, 16, 30, 6),
-        ),
-        dict(
-            package_id="04685768-e41d-49e4-9192-19b6d435226a",
-            name="a",
-            version="0.2.0",
-            status=Status.QUEUED,
-            score=None,
-            inspector_url=None,
-            queued_at=dt.datetime(2023, 5, 12, 17),
-            pending_at=None,
-            finished_at=None,
-            client_id=None,
-            reported_at=None,
-        ),
-        dict(
-            package_id="bbb953ca-95af-4d57-a7a5-0b656f652695",
-            name="b",
-            version="0.2.0",
-            status=Status.PENDING,
-            score=None,
-            inspector_url=None,
-            queued_at=dt.datetime(2023, 5, 12, 19),
-            pending_at=dt.datetime(2023, 5, 12, 20),
-            finished_at=None,
-            client_id="remmy",
-            reported_at=None,
-        ),
-    ]
+def decode(L) -> list[dict]:
+    out = []
+    for d in L:
+        new_d = d.copy()
+        for key, value in d.items():
+            if key == "status":
+                new_d[key] = Status(value)
+            elif key in ["queued_at", "pending_at", "finished_at", "reported_at"]:
+                if value is not None:
+                    new_d[key] = dt.datetime.fromisoformat(value)
+            elif key == "package_id":
+                new_d[key] = uuid.UUID(f"{{{value}}}")
+
+        out.append(new_d)
+
+    return out
+
+
+@pytest.fixture(params=TEST_DATA_FILES, ids=[p.name for p in TEST_DATA_FILES])
+def test_data(request) -> list[dict]:
+    with open(request.param) as f:
+        data = json.load(f)
+
+    return decode(data)
 
 
 @pytest.fixture(autouse=True)
-def db_setup(engine: Engine, sm: sessionmaker, test_data: list[dict]):
+def db_setup(engine: Engine, sm: sessionmaker, test_data: list[dict]) -> Generator[None, None, None]:
     Base.metadata.create_all(engine)
     with sm() as sess:
         sess.execute(insert(Package), test_data)
@@ -111,6 +88,6 @@ def db_setup(engine: Engine, sm: sessionmaker, test_data: list[dict]):
 
 
 @pytest.fixture
-def db_session(sm):
+def db_session(sm) -> Generator[Session, None, None]:
     with sm() as sess:
         yield sess
