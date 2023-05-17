1604ad431408755179f4422a3251a84308ccf4f5
jz9 <69145546+jonathan-d-zhang@users.noreply.github.com>
2023-05-16 23:21:06 -0400

    Extract and test queries

--- tests/test_job.py
+++ tests/test_job.py
@@ -1,10 +1,37 @@
 import uuid
+import datetime as dt
+import typing
 
 import requests
 from sqlalchemy import func, select
 from sqlalchemy.orm import Session
 
 from mainframe.models.orm import Package, Status
+
+
+def oldest_queued_package(db_session: Session):
+    return db_session.scalar(select(func.min(Package.queued_at)).where(Package.status == Status.QUEUED))
+
+
+def test_min_queue_date_of_queued_rows(test_data: list[dict], db_session: Session):
+    t = list(d["queued_at"] for d in test_data if d["status"] is Status.QUEUED)
+    if t:
+        assert min(t) == oldest_queued_package(db_session)
+    else:
+        # no queued rows to get the min of
+        pass
+
+
+def fetch_pid_and_queue_time(name: str, version: str, db_session: Session) -> tuple[uuid.UUID, dt.datetime]:
+    t = db_session.execute(
+        select(Package.package_id, Package.queued_at).where((Package.name == name) & (Package.version == version))
+    ).first()
+    return typing.cast(tuple[uuid.UUID, dt.datetime], t)
+
+
+def test_fetch_pid_and_queue_time(test_data: list[dict], db_session: Session):
+    for d in test_data:
+        assert (d["package_id"], d["queued_at"]) == fetch_pid_and_queue_time(d["name"], d["version"], db_session)
 
 
 def test_job(api_url: str, test_data: list[dict], db_session: Session):
@@ -14,14 +41,8 @@ def test_job(api_url: str, test_data: list[dict], db_session: Session):
     if "package_id" in j:
         # if job, the row with the name and version we get should be pending
         # and the queued_at should be at least as old as all queued packages
-        pid, job_queued_at = db_session.execute(
-            select(Package.package_id, Package.queued_at).where(
-                (Package.name == j["name"]) & (Package.version == j["version"])
-            )
-        ).first()  # type: ignore
-        oldest_still_queued = db_session.scalar(
-            select(func.min(Package.queued_at)).where(Package.status == Status.QUEUED)
-        )
+        pid, job_queued_at = fetch_pid_and_queue_time(j["name"], j["version"], db_session)
+        oldest_still_queued = oldest_queued_package(db_session)
         assert uuid.UUID(f"{{{j['package_id']}}}") == pid and (
             oldest_still_queued is None or job_queued_at >= oldest_still_queued
         )
