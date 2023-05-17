e561b7869c0e3f78804a0aa331302f2e2bd609fd
jz9 <69145546+jonathan-d-zhang@users.noreply.github.com>
2023-05-16 22:51:23 -0400

    Rewrite `test_job.py` to test using both test_data

--- tests/test_job.py
+++ tests/test_job.py
@@ -1,22 +1,30 @@
+import uuid
+
 import requests
+from sqlalchemy import func, select
+from sqlalchemy.orm import Session
+
+from mainframe.models.orm import Package, Status
 
 
-def test_acquire_job(api_url: str):
+def test_job(api_url: str, test_data: list[dict], db_session: Session):
     r = requests.post(f"{api_url}/job")
     r.raise_for_status()
-    assert r.json()["package_id"] == "04685768-e41d-49e4-9192-19b6d435226a"
-
-
-def test_no_jobs(api_url: str):
-    requests.post(f"{api_url}/job")
-    r = requests.post(f"{api_url}/job")
-    r.raise_for_status()
-    assert r.json()["detail"] == "No available packages to scan. Try again later."
-
-
-def test_entry_correctly_altered(api_url: str):
-    r = requests.post(f"{api_url}/job")
-    r.raise_for_status()
-
-    r = requests.get(f"{api_url}/package?name=a&version=0.2.0")
-    assert r.json()[0]["status"] == "pending"
+    j = r.json()
+    if "package_id" in j:
+        # if job, the row with the name and version we get should be pending
+        # and the queued_at should be at least as old as all queued packages
+        pid, job_queued_at = db_session.execute(
+            select(Package.package_id, Package.queued_at).where(
+                (Package.name == j["name"]) & (Package.version == j["version"])
+            )
+        ).first()  # type: ignore
+        oldest_still_queued = db_session.scalar(
+            select(func.min(Package.queued_at)).where(Package.status == Status.QUEUED)
+        )
+        assert uuid.UUID(f"{{{j['package_id']}}}") == pid and (
+            oldest_still_queued is None or job_queued_at >= oldest_still_queued
+        )
+    else:
+        # if no job, there must be no queued packages
+        assert all(d["status"] != "queued" for d in test_data)
