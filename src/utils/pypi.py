3ae27295b73e04697d71bb758aaf2ec1d2afb2c6
Robin5605 <robinjefferson123@gmail.com>
2023-05-18 21:55:20 -0500

    Make pypi utils readable

--- src/utils/pypi.py
+++ src/utils/pypi.py
@@ -6,5 +6,9 @@ from urllib.parse import urlparse
 def file_path_from_inspector_url(inspector_url: str) -> str:
     """Parse the file path out of a PyPI inspector URL"""
 
-    s = "/"
-    return s.join(urlparse(inspector_url).path.strip(s).split(s)[8:])
+    parsed_url = urlparse(inspector_url)
+    path = parsed_url.path.strip("/")
+    segments = path.split("/")
+
+    # The 8th element of path is when the file path starts
+    return "/".join(segments[8:])
