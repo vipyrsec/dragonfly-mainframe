a7175747365b82b7e040edc5ee2785f8556d3f74
Robin5605 <robinjefferson123@gmail.com>
2023-05-20 20:59:24 -0500

    Return download URLs in get job endpoint

--- src/mainframe/endpoints/job.py
+++ src/mainframe/endpoints/job.py
@@ -1,10 +1,13 @@
 from datetime import datetime
+from typing import Annotated
 
 from fastapi import APIRouter, Depends
+from letsbuilda.pypi import PyPIServices
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from mainframe.database import get_db
+from mainframe.dependencies import get_pypi_client
 from mainframe.models.orm import Package, Status
 from mainframe.models.schemas import JobResult, NoJob
 
@@ -12,7 +15,9 @@ router = APIRouter()
 
 
 @router.post("/job")
-async def get_job(session: AsyncSession = Depends(get_db)) -> JobResult | NoJob:
+async def get_job(
+    session: Annotated[AsyncSession, Depends(get_db)], pypi_client: Annotated[PyPIServices, Depends(get_pypi_client)]
+) -> JobResult | NoJob:
     """Request a job to work on."""
 
     query = select(Package).where(Package.status == Status.QUEUED).order_by(Package.queued_at)
@@ -26,5 +31,8 @@ async def get_job(session: AsyncSession = Depends(get_db)) -> JobResult | NoJob:
     package.pending_at = datetime.utcnow()
     await session.commit()
 
+    package_metadata = await pypi_client.get_package_metadata(package.name, package.version)
+    distribution_urls = [distribution.url for distribution in package_metadata.urls]
+
     # FIXME: Add other package data needed by the client.
-    return JobResult(package_id=package.package_id, name=package.name, version=package.version)
+    return JobResult(name=package.name, version=package.version, distributions=distribution_urls)
