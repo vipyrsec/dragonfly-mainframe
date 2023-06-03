b953a96566607599908e3648403798174cfd4a89
jz9 <69145546+jonathan-d-zhang@users.noreply.github.com>
2023-06-03 17:09:56 -0400

    Order timed-out packages after newly queued ones

--- src/mainframe/endpoints/job.py
+++ src/mainframe/endpoints/job.py
@@ -6,6 +6,7 @@ from sqlalchemy import or_, select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
 
+from mainframe.constants import mainframe_settings
 from mainframe.database import get_db
 from mainframe.models.orm import Package, Status
 from mainframe.models.schemas import JobResult, NoJob
@@ -15,17 +16,26 @@ router = APIRouter()
 
 @router.post("/job")
 async def get_job(session: Annotated[AsyncSession, Depends(get_db)], request: Request) -> JobResult | NoJob:
-    """Request a job to work on."""
+    """
+    Request a release to work on.
+
+    Clients are assigned the oldest release in the queue, i.e., the release
+    with the oldest `queued_at` time.
+
+    We also consider releases with a `pending_at` older than
+    `now() - JOB_TIMEOUT` to be queued at the current time. This way, timed out
+    packages are always processed after newly queued packages.
+    """
 
     scalars = await session.scalars(
         select(Package)
         .where(
             or_(
                 Package.status == Status.QUEUED,
-                Package.pending_at < datetime.utcnow() - timedelta(minutes=2),
+                Package.pending_at < datetime.utcnow() - timedelta(seconds=mainframe_settings.job_timeout),
             )
         )
-        .order_by(Package.pending_at, Package.queued_at)
+        .order_by(Package.pending_at.nulls_first(), Package.queued_at)
         .options(selectinload(Package.download_urls))
     )
     package = scalars.first()
