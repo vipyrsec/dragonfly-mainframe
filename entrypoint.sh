5169ee41f5b0e1101e77a949e2cc67abd69d7ef3
Bradley Reynolds <bradley.reynolds@darbia.dev>
2023-06-08 13:37:48 -0500

    remove heckery
    Signed-off-by: Bradley Reynolds <bradley.reynolds@darbia.dev>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python -m alembic upgrade head && python -m uvicorn src.mainframe.__main__:app && exec "$0"
+python -m alembic upgrade head && python -m uvicorn mainframe.__main__:app && exec "$0"
