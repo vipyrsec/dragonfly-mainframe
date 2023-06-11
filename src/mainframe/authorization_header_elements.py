cffc6eac5c28d9fb7cc1852b12f0d7e013437edc
Robin5605 <robinjefferson123@gmail.com>
2023-06-10 22:23:22 -0500

    Disable auth in dev instances

--- src/mainframe/authorization_header_elements.py
+++ src/mainframe/authorization_header_elements.py
@@ -1,3 +1,4 @@
+from os import getenv
 from typing import NamedTuple
 
 from starlette.requests import Request as StarletteRequest
@@ -27,6 +28,9 @@ def get_authorization_header_elements(
 
 
 def get_bearer_token(request: StarletteRequest) -> str:
+    if getenv("GIT_SHA", "development") == "development":
+        return ""
+
     authorization_header = request.headers.get("Authorization")
     if authorization_header:
         authorization_header_elements = get_authorization_header_elements(authorization_header)
