030008be082a31f6650103f7abc47423797de1bc
Robin5605 <robinjefferson123@gmail.com>
2023-07-08 19:20:30 -0500

    Fix tests

--- tests/test_job.py
+++ tests/test_job.py
@@ -1,6 +1,4 @@
 import datetime as dt
-import typing
-import uuid
 
 import requests
 from sqlalchemy import func, select
@@ -22,37 +20,33 @@ def test_min_queue_date_of_queued_rows(test_data: list[dict], db_session: Sessio
         pass
 
 
-def fetch_pid_and_queue_time(name: str, version: str, db_session: Session) -> tuple[uuid.UUID, dt.datetime]:
-    t = db_session.execute(
-        select(Scan.scan_id, Scan.queued_at).where((Scan.name == name) & (Scan.version == version))
-    ).first()
-    return typing.cast(tuple[uuid.UUID, dt.datetime], t)
+def fetch_queue_time(name: str, version: str, db_session: Session) -> dt.datetime | None:
+    return db_session.scalar(select(Scan.queued_at).where(Scan.name == name).where(Scan.version == version))
 
 
 def test_fetch_pid_and_queue_time(test_data: list[dict], db_session: Session):
     for d in test_data:
-        assert (d["scan_id"], d["queued_at"]) == fetch_pid_and_queue_time(d["name"], d["version"], db_session)
+        assert d["queued_at"] == fetch_queue_time(d["name"], d["version"], db_session)
 
 
 def test_job(api_url: str, test_data: list[dict], db_session: Session):
-    r = requests.post(f"{api_url}/job")
+    r = requests.post(f"{api_url}/jobs")
     r.raise_for_status()
     j = r.json()
-    if "package_id" in j:
+    if j:
+        j = j[0]
         # if job, the row with the name and version we get should be pending
         # and the queued_at should be at least as old as all queued packages
-        pid, job_queued_at = fetch_pid_and_queue_time(j["name"], j["version"], db_session)
+        job_queued_at = fetch_queue_time(j["name"], j["version"], db_session)
         oldest_still_queued = oldest_queued_package(db_session)
-        assert uuid.UUID(f"{{{j['package_id']}}}") == pid and (
-            oldest_still_queued is None or job_queued_at >= oldest_still_queued
-        )
+        assert oldest_still_queued is None or job_queued_at >= oldest_still_queued
     else:
         # if no job, there must be no queued packages
         assert all(d["status"] != "queued" for d in test_data)
 
 
 def test_batch_job(api_url: str, test_data: list[dict], db_session: Session):
-    r = requests.get(f"{api_url}/batch/job", params=dict(n_jobs=len(test_data)))
+    r = requests.post(f"{api_url}/jobs", params=dict(n_jobs=len(test_data)))
     r.raise_for_status()
     j = r.json()
 
