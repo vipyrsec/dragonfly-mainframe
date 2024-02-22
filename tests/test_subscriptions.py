d7508f04a4f18fccf5fb71a19284e3e8fc849627
Robin5605 <robinjefferson123@gmail.com>
2024-02-22 17:10:33 -0600

    Add test to subscribe to nonexistent package

--- tests/test_subscriptions.py
+++ tests/test_subscriptions.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+import pytest
+from fastapi import HTTPException
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
@@ -28,6 +30,17 @@ def test_create_subscription(db_session: Session):
     package = db_session.scalar(select(Package).where(Package.name == "test-package"))
     assert package is not None
     assert package.people[0].discord_id == discord_id
+
+
+def test_create_subscription_nonexistent_package(db_session: Session):
+    payload = AddSubscription(
+        package_name="nonexistent-package",
+        discord_id=123456789,
+    )
+
+    with pytest.raises(HTTPException) as error:
+        create_subscription_route(payload, db_session)
+        assert error.value.status_code == 404
 
 
 def test_get_person(db_session: Session):
