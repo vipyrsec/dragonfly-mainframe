858963877bc2dfdc90f3555b12a1eb8bfeb10c97
Robin5605 <robinjefferson123@gmail.com>
2024-02-22 15:22:37 -0600

    Add tests for create and get subscription routes

---
+++ tests/test_subscriptions.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from mainframe.endpoints.subscriptions import (
+    create_subscription_route,
+    get_person_route,
+)
+from mainframe.models.orm import Package, Person, Scan, Status
+from mainframe.models.schemas import AddSubscription
+
+
+def test_create_subscription(db_session: Session):
+    package = Package(name="test-package", scans=[], people=[])
+    db_session.add(package)
+    db_session.commit()
+
+    discord_id = 123456789
+
+    payload = AddSubscription(
+        package_name=package.name,
+        discord_id=discord_id,
+    )
+
+    create_subscription_route(payload, db_session)
+
+    package = db_session.scalar(select(Package).where(Package.name == "test-package"))
+    assert package is not None
+    assert package.people[0].discord_id == discord_id
+
+
+def test_get_person(db_session: Session):
+    scan = Scan(
+        name="test-package",
+        version="1.0.0",
+        status=Status.QUEUED,
+        queued_at=datetime.now(),
+        queued_by="remmy",
+    )
+    person = Person(discord_id=123, email_address=None)
+    package = Package(name="test-package", scans=[scan], people=[person])
+
+    db_session.add(package)
+    db_session.commit()
+
+    person = get_person_route(person_id=person.id, session=db_session)
+
+    assert person.discord_id == 123
+    assert person.email_address is None
+    assert person.packages == ["test-package"]
