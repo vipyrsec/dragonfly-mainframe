323aa251533b93e5764fb5f85106b130d52139a6
Bruno Rodrigues dos Santos <130629262+BruDriguezz@users.noreply.github.com>
2024-03-06 13:54:07 -0300

    Remove `PlainSerializer`, re-add `@field_serializer`
    Signed-off-by: Bruno Rodrigues dos Santos <130629262+BruDriguezz@users.noreply.github.com>

--- src/mainframe/models/schemas.py
+++ src/mainframe/models/schemas.py
@@ -22,13 +22,6 @@ class Error(BaseModel):
 class Package(BaseModel):
     """Model representing a package queried from the database."""
 
-    _datetime = Annotated[
-        int | datetime,
-        functional_serializers.PlainSerializer(
-            lambda x: int(x.timestamp()) if isinstance(x, datetime) else print(f"{x} is not a datetime object")
-        ),
-    ]
-
     scan_id: str
     name: str
     version: str = ""
@@ -37,13 +30,13 @@ class Package(BaseModel):
     inspector_url: Optional[str] = ""
     rules: list[str] = []
     download_urls: list[str] = []
-    queued_at: Optional[_datetime]
+    queued_at: Optional[datetime]
     queued_by: str = ""
-    reported_at: Optional[_datetime]
+    reported_at: Optional[datetime]
     reported_by: Optional[str]
-    pending_at: Optional[_datetime]
+    pending_at: Optional[datetime]
     pending_by: Optional[str] = None
-    finished_at: Optional[_datetime]
+    finished_at: Optional[datetime]
     finished_by: Optional[str] = None
     commit_hash: Optional[str] = ""
 
@@ -69,15 +62,15 @@ class Package(BaseModel):
             commit_hash=scan.commit_hash,
         )
 
-    # @field_serializer(
-    #     "queued_at",
-    #     "pending_at",
-    #     "finished_at",
-    #     "reported_at",
-    # )
-    # def serialize_dt(self, dt: Optional[datetime], _info):  # pyright: ignore
-    #     if dt:
-    #         return int(dt.timestamp())
+     @field_serializer(
+         "queued_at",
+         "pending_at",
+         "finished_at",
+         "reported_at",
+     )
+     def serialize_dt(self, dt: Optional[datetime], _info):  # pyright: ignore
+         if dt:
+             return int(dt.timestamp())
 
 
 class PackageSpecifier(BaseModel):
