6f7ded7bd7d6b82e3baaa28687a4d3f6e833db88
Robin5605 <robinjefferson123@gmail.com>
2024-03-13 11:22:46 -0500

    Use Keycloak when validating JWTs

--- src/mainframe/json_web_token.py
+++ src/mainframe/json_web_token.py
@@ -4,12 +4,17 @@ from typing import Any
 
 import jwt
 
-from mainframe.constants import mainframe_settings
+from mainframe.constants import keycloak_settings
 from mainframe.custom_exceptions import (
     BadCredentialsException,
     UnableCredentialsException,
 )
 
+from urllib.request import build_opener, install_opener
+
+opener = build_opener()
+opener.addheaders = [('User-Agent','Dragonfly Mainframe')]
+install_opener(opener)
 
 @dataclass
 class AuthenticationData:
@@ -18,7 +23,6 @@ class AuthenticationData:
     audience: str
     issued_at: datetime
     expires_at: datetime
-    grant_type: str
 
     @classmethod
     def from_dict(cls, data: dict[Any, Any]):
@@ -28,7 +32,6 @@ class AuthenticationData:
             audience=data["aud"],
             issued_at=datetime.fromtimestamp(data["iat"]),
             expires_at=datetime.fromtimestamp(data["exp"]),
-            grant_type=data["gty"],
         )
 
 
@@ -37,22 +40,20 @@ class JsonWebToken:
     """Perform JSON Web Token (JWT) validation using PyJWT"""
 
     jwt_access_token: str
-    auth0_issuer_url: str = f"https://{mainframe_settings.auth0_domain}/"
-    auth0_audience: str = mainframe_settings.auth0_audience
     algorithm: str = "RS256"
-    jwks_uri: str = f"{auth0_issuer_url}.well-known/jwks.json"
 
     def validate(self) -> AuthenticationData:
         try:
-            jwks_client = jwt.PyJWKClient(self.jwks_uri)
+            jwks_client = jwt.PyJWKClient(keycloak_settings.jwks_uri)
             jwt_signing_key = jwks_client.get_signing_key_from_jwt(self.jwt_access_token).key
             payload = jwt.decode(
                 self.jwt_access_token,
                 jwt_signing_key,
                 algorithms=self.algorithm,  # type: ignore
-                audience=self.auth0_audience,
-                issuer=self.auth0_issuer_url,
+                audience=keycloak_settings.audience,
+                issuer=keycloak_settings.issuer_url,
             )
+            print(payload)
         except jwt.exceptions.PyJWKClientError:
             raise UnableCredentialsException
         except jwt.exceptions.InvalidTokenError:
