da1b09d157c477e7c73a6cd961742934f215a7f0
jonathan-d-zhang <69145546+jonathan-d-zhang@users.noreply.github.com>
2024-04-08 15:09:55 -0400

    feat: Add configuration options for conn pool size
    I don't like the way SQLA has an option that sounds like max size and an
    overflow size; it seems unintuitive to exceed the pool_size.

--- src/mainframe/database.py
+++ src/mainframe/database.py
@@ -7,7 +7,11 @@ from mainframe.constants import mainframe_settings
 
 # pool_size and max_overflow are set to their default values. There is never
 # enough load to justify increasing them.
-engine = create_engine(mainframe_settings.db_url, pool_size=5, max_overflow=10)
+engine = create_engine(
+    mainframe_settings.db_url,
+    pool_size=mainframe_settings.db_connection_pool_persistent_size,
+    max_overflow=mainframe_settings.db_connection_pool_max_size - mainframe_settings.db_connection_pool_persistent_size,
+)
 sessionmaker = sessionmaker(bind=engine, expire_on_commit=False)
 
 
