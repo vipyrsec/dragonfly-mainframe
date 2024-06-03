034f3e70ba314e5272f5afee2cdd62abb0dcafeb
Robin5605 <robinjefferson123@gmail.com>
2024-06-03 12:40:53 -0500

    Configure logfire at module level

--- src/mainframe/server.py
+++ src/mainframe/server.py
@@ -22,9 +22,10 @@ from mainframe.rules import Rules, fetch_rules
 
 from . import __version__
 
+logfire.configure(send_to_logfire="if-token-present")
+
 
 def configure_logger():
-    logfire.configure(send_to_logfire="if-token-present")
     logfire.instrument_httpx()
 
     # Define the shared processors, regardless of whether API is running in prod or dev.
