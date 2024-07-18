646edb7c03ceb93ff0c3d0c095051bd75318daea
Robin5605 <robinjefferson123@gmail.com>
2024-07-17 20:54:38 -0500

    Add toggleable JSON logging

--- src/mainframe/server.py
+++ src/mainframe/server.py
@@ -49,17 +49,10 @@ def configure_logger():
         cache_logger_on_first_use=True,
     )
 
-    # log_renderer: structlog.types.Processor
-    # # If running in production, render logs with JSON.
-    # if mainframe_settings.production:
-    #     log_renderer = structlog.processors.JSONRenderer()
-    # else:
-    #     # If running in a development environment, pretty print logs
-    #     log_renderer = structlog.dev.ConsoleRenderer()
-
-    # TODO: Once infra for log aggregation is up and running, remove this and go back to
-    # TODO: JSON logging in production.
-    log_renderer = structlog.dev.ConsoleRenderer(colors=False)
+    if GIT_SHA in ("development", "testing"):
+        log_renderer = structlog.dev.ConsoleRenderer(colors=False)
+    else:
+        log_renderer = structlog.processors.JSONRenderer()
 
     formatter = structlog.stdlib.ProcessorFormatter(
         foreign_pre_chain=shared_processors,
