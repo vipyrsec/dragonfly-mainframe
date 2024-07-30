6a4db305f6fa8144d963c9c67064c98b78df05f0
Robin5605 <robinjefferson123@gmail.com>
2024-07-29 22:56:58 -0500

    Add utility to bridge Pydantic models and SQLAlchemy ORM
    Use SQLAlchemy's TypeDecorator to build a wrapper that can serialize and
    deserialize from a Pydantic model into a PostgreSQL JSONB column

--- src/mainframe/models/__init__.py
+++ src/mainframe/models/__init__.py
@@ -1,1 +1,26 @@
 """Database models."""
+
+from typing import Optional, Any, Type
+from pydantic import BaseModel
+from sqlalchemy import Dialect, TypeDecorator
+from sqlalchemy.dialects.postgresql import JSONB
+
+
+class Pydantic[T: BaseModel](TypeDecorator[T]):
+    """TypeDecorator to convert between Pydantic models and JSONB."""
+
+    impl = JSONB
+
+    def __init__(self, pydantic_type: Type[T]):
+        super().__init__()
+        self.PydanticType = pydantic_type
+
+    def process_bind_param(self, value: Optional[T], dialect: Dialect) -> dict[str, Any]:
+        if value:
+            return value.model_dump()
+        else:
+            return {}
+
+    def process_result_value(self, value: Any, dialect: Dialect) -> Optional[T]:
+        if value:
+            return self.PydanticType.model_validate(value)
