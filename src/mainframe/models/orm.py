9b4b2e730b414cafe97bc25da9be0b77eb5ef2d7
Siddhesh Mhadnak <siddhesh.mhadnak@outlook.com>
2025-03-11 02:26:49 +0530

    fix: resolve type checker errors
    This commit resolves type checker errors reported by Pyright.

--- src/mainframe/models/orm.py
+++ src/mainframe/models/orm.py
@@ -47,8 +47,8 @@ class Status(Enum):
 package_rules = Table(
     "package_rules",
     Base.metadata,
-    Column("scan_id", ForeignKey("scans.scan_id")),
-    Column("rule_id", ForeignKey("rules.id")),
+    Column("scan_id", ForeignKey("scans.scan_id")),  # pyright: ignore[reportUnknownArgumentType]
+    Column("rule_id", ForeignKey("rules.id")),  # pyright: ignore[reportUnknownArgumentType]
     PrimaryKeyConstraint("scan_id", "rule_id"),
 )
 
