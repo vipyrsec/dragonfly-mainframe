ad247a02ade56f5567a8e78d863b19dcbcf0164c
Rem <import-pandas-as-numpy@users.noreply.github.com>
2026-07-23 23:07:14 -0400

    Harden Cloudflare JWT key validation

--- src/mainframe/json_web_token.py
+++ src/mainframe/json_web_token.py
@@ -1,5 +1,8 @@
 import datetime as dt
+import time
 from dataclasses import dataclass
+from functools import cache
+from threading import Lock
 from typing import Any, Self
 
 import jwt
@@ -9,6 +12,45 @@ from mainframe.custom_exceptions import (
     BadCredentialsException,
     UnableCredentialsException,
 )
+
+
+class RateLimitedPyJWKClient(jwt.PyJWKClient):
+    """Allow key-rotation refreshes without an unknown-key request stampede."""
+
+    def __init__(self, uri: str, refresh_interval: float = 60.0) -> None:
+        super().__init__(uri)
+        self._refresh_interval = refresh_interval
+        self._next_refresh_at = 0.0
+        self._refresh_lock = Lock()
+
+    def get_signing_key(self, kid: str) -> jwt.PyJWK:
+        signing_key = self.match_kid(self.get_signing_keys(), kid)
+        if signing_key is not None:
+            return signing_key
+
+        with self._refresh_lock:
+            signing_key = self.match_kid(self.get_signing_keys(), kid)
+            if signing_key is not None:
+                return signing_key
+
+            now = time.monotonic()
+            if now < self._next_refresh_at:
+                message = "Unable to find a matching signing key"
+                raise jwt.exceptions.PyJWKClientError(message)
+
+            self._next_refresh_at = now + self._refresh_interval
+            signing_key = self.match_kid(self.get_signing_keys(refresh=True), kid)
+            if signing_key is None:
+                message = "Unable to find a matching signing key"
+                raise jwt.exceptions.PyJWKClientError(message)
+
+            return signing_key
+
+
+@cache
+def get_jwks_client(jwks_uri: str) -> jwt.PyJWKClient:
+    """Return a shared JWKS client with PyJWT's key-set cache."""
+    return RateLimitedPyJWKClient(jwks_uri)
 
 
 @dataclass
@@ -43,7 +85,7 @@ class JsonWebToken:
 
     def validate(self) -> AuthenticationData:
         try:
-            jwks_client = jwt.PyJWKClient(self.jwks_uri)
+            jwks_client = get_jwks_client(self.jwks_uri)
             jwt_signing_key = jwks_client.get_signing_key_from_jwt(self.jwt_access_token).key
 
             payload = jwt.decode(
@@ -53,9 +95,9 @@ class JsonWebToken:
                 issuer=cf_access_settings.team_domain,
                 algorithms=[self.algorithm],
             )
-        except jwt.exceptions.PyJWKClientError as err:
+        except jwt.exceptions.PyJWKClientConnectionError as err:
             raise UnableCredentialsException from err
-        except jwt.exceptions.InvalidTokenError as err:
+        except (jwt.exceptions.PyJWKClientError, jwt.exceptions.InvalidTokenError) as err:
             raise BadCredentialsException from err
 
         auth_data = AuthenticationData.from_dict(payload)
